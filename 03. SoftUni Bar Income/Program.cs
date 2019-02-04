2193e0a2604762c8220eeaf7105e6e55c231c135
Atanas Kolev <44707978+naskomsm@users.noreply.github.com>
2019-02-04 12:50:22 +0200

    Add files via upload

---
+++ 03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleCatalogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double totalIncome = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end of shift")
+                {
+                    break;
+                }
+
+                // dont look at this :D:D:D:D:D
+                int start1 = input.IndexOf('%');
+                int start2 = input.IndexOf('<');
+                int end2 = input.IndexOf('>');
+                int start3 = input.IndexOf('|');
+                int start4 = input.LastIndexOf('|') + 1;
+                int end4 = input.IndexOf('$');
+
+                string customer = input.Substring(start1, input.LastIndexOf('%') - start1).Trim('%');
+                string product = input.Substring(start2, end2 - start2).Trim('<');
+                int count = int.Parse(input.Substring(start3, input.LastIndexOf('|') - start3).Trim('|'));
+                double price = double.Parse(input.Substring(start4, end4 - start4));
+
+                double totalPrice = count * price;
+                totalIncome += totalPrice;
+                Console.WriteLine($"{customer}: {product} - {totalPrice:f2}");
+            }
+            Console.WriteLine($"Total income: {totalIncome:f2}");
+        }
+    }
+}
---
+++ 07. SoftUniExams/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07._Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> Results = new Dictionary<string, int>();
+            Dictionary<string, int> Submissions = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "exam finished")
+                {
+                    break;
+                }
+                string[] splittedInput = input.Split("-");
+
+                if (splittedInput.Length == 3)
+                {
+                    string userName = splittedInput[0];
+                    string language = splittedInput[1];
+                    int points = int.Parse(splittedInput[2]);
+
+                    if (!Results.ContainsKey(userName))
+                    {
+                        Results.Add(userName, points);
+                    }
+                    else
+                    {
+                        if (Results[userName] < points)
+                        {
+                            Results[userName] = points;
+                        }
+                    }
+
+                    if (!Submissions.ContainsKey(language))
+                    {
+                        Submissions.Add(language, 1);
+                    }
+                    else
+                    {
+                        Submissions[language]++;
+                    }
+
+                }
+                else // its command
+                {
+                    string userName = splittedInput[0];
+                    string command = splittedInput[1];
+                    if (command == "banned")
+                    {
+                        Results.Remove(userName);
+                    }
+                }
+            }
+
+            Results = Results.OrderBy(x => x.Key).OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            Submissions = Submissions.OrderByDescending(x => x.Value).OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+            Console.WriteLine($"Results: ");
+            foreach (var kvp in Results)
+            {
+                Console.WriteLine($"{kvp.Key} | {kvp.Value}");
+            }
+            Console.WriteLine($"Submissions: ");
+            foreach (var kvp in Submissions)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
+        }
+    }
+}
